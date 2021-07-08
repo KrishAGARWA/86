15c45b169f5837504e9440371f604b2d1db1c61b
KrishAGARWA <73591403+KrishAGARWA@users.noreply.github.com>
2021-07-08 20:44:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBbcBxQDhK2SXRsmJzj6OcCIYzdD5rzn4k",
+    authDomain: "spectrum-20e17.firebaseapp.com",
+    projectId: "spectrum-20e17",
+    storageBucket: "spectrum-20e17.appspot.com",
+    messagingSenderId: "64845663761",
+    appId: "1:64845663761:web:91ee5dfde1e817ed72abb0"
+  };
